2efb77f6dbc71a7d3f97a58b4d8e5080941d38e9
Cesar Alcancio <cesar.alcancio@payclip.com>
2021-01-26 21:37:53 -0600

    First web app in clojure following the documentation: https://github.com/ring-clojure/ring/wiki/Getting-Started and https://github.com/technomancy/leiningen/blob/master/doc/TUTORIAL.md

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject my-stuff "0.1.0-SNAPSHOT"
   :description "My first project"
-  :url "http://example.com/FIXME"
+  :url "http://cesar.alcancio.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
                  [org.clojure/clojure "1.10.0"]
                  [clj-http "2.0.0"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]
                  ]
   :main ^:skip-aot my-stuff.core
   :target-path "target/%s"
